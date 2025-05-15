5cd199ce3cc8caa845d89ec652833391b092677b
Alex Pu <276558492@qq.com>
2025-05-16 00:17:51 +0800

    一些配置

--- app.go
+++ app.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"fmt"
+
+	"github.com/wailsapp/wails/v2/pkg/runtime"
 )
 
 // App struct
@@ -19,6 +21,35 @@ func NewApp() *App {
 // so we can call the runtime methods
 func (a *App) startup(ctx context.Context) {
 	a.ctx = ctx
+	runtime.LogInfo(ctx, "项目启动成功")
+}
+
+func (a *App) shutdown(ctx context.Context) {
+	runtime.LogInfo(ctx, "项目关闭成功")
+}
+
+func (a *App) beforeClose(ctx context.Context) (prevent bool) {
+	dialog, err := runtime.MessageDialog(ctx, runtime.MessageDialogOptions{
+		Type:          runtime.QuestionDialog,
+		Title:         "提示",
+		Message:       "是否确定退出？",
+		Buttons:       []string{"取消", "确定退出"},
+		DefaultButton: "取消",
+	})
+
+	if err != nil {
+		runtime.LogFatalf(ctx, "关闭应用程序时，出错： %s", err.Error())
+	}
+
+	if dialog == "确定退出" {
+		envInfo := runtime.Environment(ctx)
+		runtime.LogInfof(ctx, "关闭应用程序，环境信息：%+v", envInfo)
+	}
+
+	// 如果用户点击了“取消”，则阻止应用程序关闭
+	prevent = "确定退出" != dialog
+
+	return
 }
 
 // Greet returns a greeting for the given name
--- main.go
+++ main.go
@@ -4,8 +4,10 @@ import (
 	"embed"
 
 	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/logger"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"github.com/wailsapp/wails/v2/pkg/options/mac"
 )
 
 //go:embed all:frontend/dist
@@ -17,14 +19,28 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "微信公众号图文抓手",
-		Width:  1024,
-		Height: 768,
+		Title:                            "小绿书爬虫",
+		Width:                            1024,
+		MinWidth:                         1000,
+		Height:                           768,
+		MinHeight:                        600,
+		HideWindowOnClose:                true,        // 关闭窗口时隐藏而不是退出
+		EnableFraudulentWebsiteDetection: true,        // 启用针对欺诈内容（例如恶意软件或网络钓鱼尝试）的扫描服务
+		LogLevel:                         logger.INFO, // 日志级别
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
 		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
-		OnStartup:        app.startup,
+		OnStartup:        app.startup,     // 创建窗口并即将开始加载前端资源时的回调
+		OnShutdown:       app.shutdown,    // 应用程序即将退出时的回调
+		OnBeforeClose:    app.beforeClose, // 应用关闭前的回调
+		Mac: &mac.Options{
+			About: &mac.AboutInfo{
+				Title:   "wxGraphCrawler",
+				Message: "专用于抓取微信公众号图文类型（俗称：小绿书）中的图片小工具 \r\n @Copyright 2025 by Alex",
+				Icon:    nil,
+			},
+		},
 		Bind: []interface{}{
 			app,
 		},
